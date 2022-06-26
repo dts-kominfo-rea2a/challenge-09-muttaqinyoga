00222c67c604a2c49bf1c9c82f6d4cb129586b6d
muttaqinyoga <muttaqinyoga@gmail.com>
2022-06-26 23:42:32 +0700

    Finish

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,17 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = (c) => {
     return (
-        <></>
+        <div className='card'>
+            <img src={c.data.photo} alt={c.data.name}></img>
+            <div className='container'>
+                <h1>{c.data.name}</h1>
+                <h4>{c.data.phone}</h4>
+                <h4>{c.data.email}</h4>
+            </div>
+        </div>
+        
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
 
-const Header = () => {
+const Header = (h) => {
     return (
-        <></>
+        <div className='header'>
+            <h1>call a friend</h1>
+            <h5>Your friendly contact app</h5>
+            <div className='dot'>
+                ...........
+            </div>
+        </div>
     )
 }
 
